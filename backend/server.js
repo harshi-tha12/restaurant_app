afa881185fcc593d365ea9647393bdbeb24fa212
ashu-harshi <raoharshitha@gmail.com>
2026-08-04 21:24:17 +0530

    Updatecode

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,25 @@ const db = require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://foodierestqr.netlify.app',
+  'https://www.foodierestqr.netlify.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.options('*', cors());
 // Serve uploaded images
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
